21d6d706e46f77d2600077df527034afb5c2c048
CodeGod27 <100406484+CodeGod27@users.noreply.github.com>
2024-03-14 11:33:45 +0530

    Add files via upload

---
+++ RideSelector.js
@@ -0,0 +1,106 @@
+// import Image from 'next/image'
+// import ethLogo from '../assets/eth-logo.png'
+// import { useEffect, useContext, useState } from 'react'
+// import { UberContext } from '../context/uberContext'
+import uberX from '../assets/rides/uberX.png'
+import uberBlack from '../assets/rides/uberBlack.png'
+import uberblackSuv from '../assets/rides/uberBlackSuv.png'
+import uberXL from '../assets/rides/uberXL.png'
+import uberSelect from '../assets/rides/uberSelect.png'
+import ethLogo from '../assets/eth-logo.png'
+import Image from 'next/image'
+const carList =[
+    {
+        Service:'UberX' ,
+        iconUrl: uberX , 
+        priceMultiplier:1 ,
+    },
+    {
+        Service:'UberBlack' ,
+        iconUrl: uberBlack , 
+        priceMultiplier:1.5 , 
+    },
+    {
+        Service:'UberBlackSuv' ,
+        iconUrl: uberblackSuv , 
+        priceMultiplier:1.5 ,
+    },
+    {
+        Service:'UberSelect' ,
+        iconUrl: uberSelect , 
+        priceMultiplier:1.5 ,
+    },
+    {
+        Service:'UberXL' ,
+        iconUrl: uberXL , 
+        priceMultiplier:1.5 ,
+    },
+    
+]
+
+const basePrice= 1542
+const style = {
+  wrapper: `h-full flex flex-col`,
+  title: `text-gray-500 text-center text-xs py-2 border-b`,
+  carList: `flex flex-col flex-1 overflow-scroll`,
+  car: `flex p-3 m-2 items-center border-2 border-white`,
+  selectedCar: `border-2 border-black flex p-3 m-2 items-center`,
+  carImage: `h-14`,
+  carDetails: `ml-2 flex-1`,
+  service: `font-medium`,
+  time: `text-xs text-blue-500`,
+  priceContainer: `flex items-center`,
+  price: `mr-[-0.8rem]`,
+}
+
+const RideSelector = () => {
+//   const [carList, setCarList] = useState([])
+//   const { selectedRide, setSelectedRide, setPrice, basePrice } =
+//     useContext(UberContext)
+
+//   console.log(basePrice)
+
+//   useEffect(() => {
+//     ;(async () => {
+//       try {
+//         const response = await fetch('/api/db/getRideTypes')
+
+//         const data = await response.json()
+//         setCarList(data.data)
+//         setSelectedRide(data.data[0])
+//       } catch (error) {
+//         console.error(error)
+//       }
+//     })()
+//   }, [])
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.title}>Choose a ride, or swipe up for more</div>
+      <div className={style.carList}>
+        {carList.map((car, index) => (
+            <div className={style.car}>
+            <Image
+              src={car.iconUrl}
+              className={style.carImage}
+              height={50}
+              width={50}
+            />
+            <div className={style.carDetails}>
+              <div className={style.service}>{car.Service}</div>
+              <div className={style.time}>5 min away</div>
+            </div>
+            <div className={style.priceContainer}>
+              <div className={style.price}>
+                {((basePrice / 10 ** 5) * car.priceMultiplier).toFixed(5)}
+              </div>
+              <Image src={ethLogo} height={25} width={40} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default RideSelector
